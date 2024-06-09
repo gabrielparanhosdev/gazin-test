9ae3b85cbebca45ac033834698845b38f5830308
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-06-09 16:09:45 -0300

    feat: create first app

---
+++ backend/src/app.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+interface HttpError extends Error {
+  status?: number;
+}
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
+});
+
+export default app;
